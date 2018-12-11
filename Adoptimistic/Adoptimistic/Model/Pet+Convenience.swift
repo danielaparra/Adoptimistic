5c774c6c84b09c2d94c62aa873879251fae3e9d2
Daniela Parra <dyparra47@gmail.com>
2018-12-11 13:15:03 -0500

    Update pet core data model and pet rep model

--- Adoptimistic/Adoptimistic/Model/Pet+Convenience.swift
+++ Adoptimistic/Adoptimistic/Model/Pet+Convenience.swift
@@ -7,7 +7,25 @@
 //
 
 import Foundation
+import CoreData
 
 extension Pet {
-    
+    @discardableResult convenience init(petRep: PetRepresentation, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        
+        self.init(context: context)
+        self.age = petRep.age
+        self.animal = petRep.animal
+        self.breeds = petRep.breeds
+        self.identifier = petRep.identifier
+        self.lastUpdate = petRep.lastUpdate
+        self.mix = petRep.mix
+        self.name = petRep.name
+        self.options = petRep.options
+        self.petDescription = petRep.description
+        self.photos = petRep.photos
+        self.sex = petRep.sex
+        self.shelterId = petRep.shelterId
+        self.shelterPetId = petRep.shelterPetId
+        self.size = petRep.size
+    }
 }
--- Adoptimistic/Adoptimistic/Model/PetRepresentation.swift
+++ Adoptimistic/Adoptimistic/Model/PetRepresentation.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 struct PetRepresentation: Decodable, Equatable {
-    let age: AgeType
+    let age: String //Age Type
     let animal: String //Animal Type
     let breeds: [String]
     let description: String
@@ -21,15 +21,137 @@ struct PetRepresentation: Decodable, Equatable {
     let photos: [String]
     let sex: String
     let shelterId: String
-    let shelterPetId: String
-    let size: String
+    let shelterPetId: String?
+    let size: String //Size Type
+    
+    enum CodingKeys: String, CodingKey {
+        case options
+        case age
+        case size
+        case media
+        case id
+        case shelterPetId
+        case breeds
+        case name
+        case sex
+        case description
+        case mix
+        case shelterId
+        case lastUpdate
+        case animal
+    }
+    
+    enum OptionsCodingKeys: String, CodingKey {
+        case option
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let optionsContainer = try container.nestedContainer(keyedBy: OptionsCodingKeys.self, forKey: .options)
+        var optionContainer = try optionsContainer.nestedUnkeyedContainer(forKey: .option)
+        var optionStrings: [String] = []
+        while !optionContainer.isAtEnd {
+            let optionDictionary = try optionContainer.nestedContainer(keyedBy: TCodingKey.self)
+            let option = try optionDictionary.decode(String.self, forKey: .t)
+            optionStrings.append(option)
+        }
+        //haven't accounted for just one option or nil
+        
+        let ageContainer = try container.nestedContainer(keyedBy: TCodingKey.self, forKey: .age)
+        let age = try ageContainer.decode(String.self, forKey: .t)
+        let sizeContainer = try container.nestedContainer(keyedBy: TCodingKey.self, forKey: .size)
+        let size = try sizeContainer.decode(String.self, forKey: .t)
+        
+        //media photos
+        
+        let idContainer = try container.nestedContainer(keyedBy: TCodingKey.self, forKey: .id)
+        let identifier = try idContainer.decode(Int16.self, forKey: .t)
+        let shelterPetIdContainer = try container.nestedContainer(keyedBy: TCodingKey.self, forKey: .shelterPetId)
+        let shelterPetId = try shelterPetIdContainer.decodeIfPresent(String.self, forKey: .t) ?? nil
+        
+        //breeds one or many
+        
+        let nameContainer = try container.nestedContainer(keyedBy: TCodingKey.self, forKey: .name)
+        let name = try nameContainer.decode(String.self, forKey: .t)
+        let sexContainer = try container.nestedContainer(keyedBy: TCodingKey.self, forKey: .sex)
+        let sex = try sexContainer.decode(String.self, forKey: .t)
+        let descriptionContainer = try container.nestedContainer(keyedBy: TCodingKey.self, forKey: .description)
+        let description = try descriptionContainer.decode(String.self, forKey: .t)
+        let mixContainer = try container.nestedContainer(keyedBy: TCodingKey.self, forKey: .mix)
+        let mixString = try mixContainer.decode(String.self, forKey: .t)
+        let mix: Bool = mixString == "yes" ? true : false
+        let shelterIdContainer = try container.nestedContainer(keyedBy: TCodingKey.self, forKey: .shelterId)
+        let shelterId = try shelterIdContainer.decode(String.self, forKey: .t)
+        let lastUpdateContainer = try container.nestedContainer(keyedBy: TCodingKey.self, forKey: .lastUpdate)
+        let lastUpdate = try lastUpdateContainer.decode(Date.self, forKey: .t)
+        let animalContainer = try container.nestedContainer(keyedBy: TCodingKey.self, forKey: .animal)
+        let animal = try animalContainer.decode(String.self, forKey: .t)
+        
+        self.options = optionStrings
+        self.age = age
+        self.size = size
+        //photos
+        self.photos = [""]
+        self.identifier = identifier
+        self.shelterPetId = shelterPetId
+        //breeds
+        self.breeds = [""]
+        self.name = name
+        self.sex = sex
+        self.description = description
+        self.mix = mix
+        self.shelterId = shelterId
+        self.lastUpdate = lastUpdate
+        self.animal = animal
+    }
 }
 
 struct PetRandomResult: Decodable {
     let pet: PetRepresentation
     
+    enum CodingKeys: String, CodingKey {
+        case petfinder
+    }
+    
+    enum PetfinderCodingKeys: String, CodingKey {
+        case pet
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let petfinderContainer = try container.nestedContainer(keyedBy: PetfinderCodingKeys.self, forKey: .petfinder)
+        let pet = try petfinderContainer.decode(PetRepresentation.self, forKey: .pet)
+        
+        self.pet = pet
+    }
 }
 
-struct PetsResults: Decodable {
+struct PetsFindResult: Decodable {
     let pets: [PetRepresentation]
+    let lastOffset: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case petfinder
+    }
+    
+    enum PetfinderCodingKeys: String, CodingKey {
+        case pets
+        case lastOffset
+    }
+    
+    enum PetsCodingKeys: String, CodingKey {
+        case pet
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let petfinderContainer = try container.nestedContainer(keyedBy: PetfinderCodingKeys.self, forKey: .petfinder)
+        let lastOffsetContainer = try petfinderContainer.nestedContainer(keyedBy: TCodingKey.self, forKey: .lastOffset)
+        let lastOffset = try lastOffsetContainer.decode(Int.self, forKey: .t)
+        let petsContainer = try petfinderContainer.nestedContainer(keyedBy: PetsCodingKeys.self, forKey: .pets)
+        let pets = try petsContainer.decode([PetRepresentation].self, forKey: .pet)
+        
+        self.lastOffset = lastOffset
+        self.pets = pets
+    }
 }
