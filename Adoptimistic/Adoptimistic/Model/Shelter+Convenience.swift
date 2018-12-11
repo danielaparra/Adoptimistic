e1efe5845dc36c34ab7ece91f309cb428c828b76
Daniela Parra <dyparra47@gmail.com>
2018-12-11 11:29:00 -0500

    Add shelter convenience initializer.

--- Adoptimistic/Adoptimistic/Model/Shelter+Convenience.swift
+++ Adoptimistic/Adoptimistic/Model/Shelter+Convenience.swift
@@ -7,7 +7,22 @@
 //
 
 import Foundation
+import CoreData
 
 extension Shelter {
-    
+    @discardableResult convenience init(shelterRep: ShelterRepresentation, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        
+        self.init(context: context)
+        self.address = shelterRep.address
+        self.city = shelterRep.city
+        self.country = shelterRep.country
+        self.email = shelterRep.email
+        self.identifier = shelterRep.identifier
+        self.latitude = shelterRep.latitude
+        self.longitude = shelterRep.longitude
+        self.name = shelterRep.name
+        self.phone = shelterRep.phone
+        self.state = shelterRep.state
+        self.zipcode = shelterRep.zipcode
+    }
 }
