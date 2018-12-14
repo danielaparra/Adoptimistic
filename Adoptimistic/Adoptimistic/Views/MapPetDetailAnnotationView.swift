f43ecfdd14fb60b95c76c1539577abcf9e88ebf3
Daniela Parra <dyparra47@gmail.com>
2018-12-13 22:37:54 -0500

    Add map pet detail annotation view to display more information on map

---
+++ Adoptimistic/Adoptimistic/Views/MapPetDetailAnnotationView.swift
@@ -0,0 +1,9 @@
+//
+//  PetDetailAnnotationView.swift
+//  Adoptimistic
+//
+//  Created by Daniela Parra on 12/13/18.
+//  Copyright © 2018 Daniela Parra. All rights reserved.
+//
+
+import Foundation
